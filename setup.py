d87c888020a58e12b7bd1d1393d09fc124b68597
Ronald Sprouse <ronald@berkeley.edu>
2018-03-05 16:49:51 -0800

    Add setup.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+  name = 'pyhtk',
+  package_dir = {'pyhtk': 'python'},
+  packages = ['pyhtk'],
+  classifiers = [
+    'Intended Audience :: Science/Research',
+    'Topic :: Scientific/Engineering',
+  ]
+)
